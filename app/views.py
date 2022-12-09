cbca2e5bd9278c3ef25f4b03ab3ca9a7d4cb5556
ABUZER EMRE OSMANOĞLU <a.osmanoglu@jandarma.gov.tr>
2022-12-09 18:04:39 +0300

    index added

--- app/views.py
+++ app/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
+from .models import Event, EventStep
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    events = Event.objects.all()
+    context = {
+        'events': events,
+    }
+    return render(request, 'app/index.html', context)
+
+def detail(request, event_id):
+    event = Event.objects.get(id=event_id)
+    steps = EventStep.objects.filter(event=event)
+    context = {
+        'event': event,
+        'steps': steps,
+    }
+    return render(request, 'app/detail.html', context)
