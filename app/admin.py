20bb5ef211cdf961658f659b3e764f83801e92c1
Abuzer Emre Osmanoğlu <aeosmanoglu@icloud.com>
2022-12-10 21:32:08 +0300

    model güncelleme

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Event, EventStep
+from .models import Event, Step
 
 admin.site.register(Event)
-admin.site.register(EventStep)
+admin.site.register(Step)
 
 # TODO: Daha sonra bu dosyayı kullanarak admin panelindeki görünümü değiştireceğiz.
--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
+from datetime import datetime
 from django.db import models
 
 # TODO: Model genişletilecek.
-# TODO: Model içerisindeki gerekli alanlar için default değerler ve help_text değerleri verilecek.
-# TODO: Model isilendirmeleri için daha anlamlı isimler verilecek.
-# TODO: Sınıflar için docstringler eklenecek.
+# TODO: Viriyi kimin girdiği ve düzelttiği zaman bilgisi ile birlikte kaydedilecek.
 # TODO: TextField veri girişlerinde markdown desteği eklenecek.
 
 class Event(models.Model):
+  """
+  Olaylar için model. title, description, dateTime, solution_dateTime, detection_method, detection_by, level, type
+  """  
   LEVEL = [
       (3, 'Seviye 3'),
     ]
@@ -18,22 +20,30 @@ class Event(models.Model):
       (5, 'Tür 5'),
       (6, 'Tür 6'),
     ]
-  event_name = models.CharField('Olay', max_length=200)
-  examination = models.TextField('Ön İnceleme')
-  detection_date = models.DateTimeField('Tespit Tarihi ve Saati')
-  solution_date = models.DateTimeField('Çözüm Tarihi ve Saati', blank=True, null=True, default=None, help_text='Çözüm tarihi ve saati girilmediyse olay çözülmedi olarak kabul edilecektir.')
+  title = models.CharField('Olay', max_length=200)
+  description = models.TextField('Ön İnceleme')
+  dateTime = models.DateTimeField('Tespit Tarihi ve Saati', default=datetime.now)
+  solution_dateTime = models.DateTimeField(
+    'Çözüm Tarihi ve Saati',
+    blank=True,
+    null=True,
+    default=None,
+    help_text='Çözüm tarihi ve saati girilmediyse olay çözülmedi olarak kabul edilecektir.')
   detection_method = models.CharField('Tespit Yöntemi', max_length=200, help_text='Telefonla, SOME Ekranı, vb...')
-  first_detecter = models.CharField('İlk Tespit Eden', max_length=200, help_text='SOME Nöbetçi Heyeti')
+  detection_by = models.CharField('Tespit Eden', max_length=200, help_text='SOME Nöbetçi Heyeti')
   level = models.IntegerField(choices=LEVEL, default=3)
-  theType = models.IntegerField(choices=TYPE, default=4)
+  type = models.IntegerField(choices=TYPE, default=4)
 
   def __str__(self):
-    return self.event_name
+    return self.title
 
-class EventStep(models.Model):
+class Step(models.Model):
+  """
+  Olaylara ait adımlar için model. event, description, dateTime.
+  """
   event = models.ForeignKey(Event, on_delete=models.CASCADE)
-  step = models.TextField('Yapılan İşlemler')
-  step_date = models.DateTimeField('İşlem Tarihi ve Saati')
+  description = models.TextField('Yapılan İşlemler')
+  dateTime = models.DateTimeField('İşlem Tarihi ve Saati', default=datetime.now)
 
   def __str__(self):
-    return self.step_date
+    return self.dateTime.__str__()
