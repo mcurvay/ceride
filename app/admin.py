b72d6878a07498de3d82d9bb6b0eada127c727e6
Abuzer Emre Osmanoğlu <aeosmanoglu@icloud.com>
2022-12-11 12:43:24 +0300

    Viriyi kimin girdiği ve düzelttiği zaman bilgisi ile birlikte kaydediliyor

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,45 @@ from django.contrib import admin
 
 from .models import Event, Step
 
-admin.site.register(Event)
-admin.site.register(Step)
+
+class EventAdmin(admin.ModelAdmin):
+    # readonly_fields alanları sadece okunabilir olacak, değiştirilemeyecek
+    readonly_fields = ('created_by', 'created_at', 'updated_by', 'updated_at')
+
+    # Kaydetme işleminden önce bu fonksiyon çalışacak
+    def save_model(self, request, obj, form, change):
+        # obj.pk değeri None ise yeni bir kayıt oluşturuluyor
+        if obj.pk is None:
+            # created_by alanına veri giren kullanıcı atanıyor
+            obj.created_by = request.user
+        # obj.pk değeri None değilse var olan bir kaydın güncelleniyor
+        else:
+            # updated_by alanına veriyi güncelleyen kullanıcı atanıyor
+            obj.updated_by = request.user
+
+        # Kaydetme işlemini yap
+        super().save_model(request, obj, form, change)
+
+
+class StepAdmin(admin.ModelAdmin):
+    # readonly_fields alanları sadece okunabilir olacak, değiştirilemeyecek
+    readonly_fields = ('created_by', 'created_at', 'updated_by', 'updated_at')
+
+    # Kaydetme işleminden önce bu fonksiyon çalışacak
+    def save_model(self, request, obj, form, change):
+        # obj.pk değeri None ise yeni bir kayıt oluşturuluyor
+        if obj.pk is None:
+            # created_by alanına veri giren kullanıcı atanıyor
+            obj.created_by = request.user
+        # obj.pk değeri None değilse var olan bir kaydın güncelleniyor
+        else:
+            # updated_by alanına veriyi güncelleyen kullanıcı atanıyor
+            obj.updated_by = request.user
+
+        # Kaydetme işlemini yap
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Step, StepAdmin)
 
 # TODO: Daha sonra bu dosyayı kullanarak admin panelindeki görünümü değiştireceğiz.
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 # TODO: Model genişletilecek.
 # TODO: Viriyi kimin girdiği ve düzelttiği zaman bilgisi ile birlikte kaydedilecek.
@@ -7,7 +8,7 @@ from django.db import models
 
 class Event(models.Model):
   """
-  Olaylar için model. title, description, dateTime, solution_dateTime, detection_method, detection_by, level, type
+  Olaylar için model. title, description, dateTime, solved_at, detection_method, detection_by, level, type, created_by, created_at, updated_by, updated_at.
   """  
   LEVEL = [
       (3, 'Seviye 3'),
@@ -23,27 +24,33 @@ class Event(models.Model):
   title = models.CharField('Olay', max_length=200)
   description = models.TextField('Ön İnceleme')
   dateTime = models.DateTimeField('Tespit Tarihi ve Saati', default=datetime.now)
-  solution_dateTime = models.DateTimeField(
-    'Çözüm Tarihi ve Saati',
-    blank=True,
-    null=True,
-    default=None,
-    help_text='Çözüm tarihi ve saati girilmediyse olay çözülmedi olarak kabul edilecektir.')
+  solved_at = models.DateTimeField(
+    'Çözüm Tarihi ve Saati', blank=True, null=True, default=None,
+    help_text='Çözüm tarihi ve saati girilmediyse olay çözülmedi olarak kabul edilecektir.'
+    )
   detection_method = models.CharField('Tespit Yöntemi', max_length=200, help_text='Telefonla, SOME Ekranı, vb...')
   detection_by = models.CharField('Tespit Eden', max_length=200, help_text='SOME Nöbetçi Heyeti')
   level = models.IntegerField(choices=LEVEL, default=3)
   type = models.IntegerField(choices=TYPE, default=4)
+  created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_created_by')
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_updated_by', null=True, blank=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.title
 
 class Step(models.Model):
   """
-  Olaylara ait adımlar için model. event, description, dateTime.
+  Olaylara ait adımlar için model. event, description, dateTime, created_by, created_at, updated_by, updated_at.
   """
   event = models.ForeignKey(Event, on_delete=models.CASCADE)
   description = models.TextField('Yapılan İşlemler')
   dateTime = models.DateTimeField('İşlem Tarihi ve Saati', default=datetime.now)
+  created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_created_by')
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_updated_by', null=True, blank=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.dateTime.__str__()
