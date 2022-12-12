1566b320772e4dd6635fd56c48f54ddccc41a51a
mcurvay <96953565+mcurvay@users.noreply.github.com>
2022-12-12 14:24:48 +0300

    admin panelde readonly özellikler çıkarıldı

--- app/admin.py
+++ app/admin.py
@@ -7,16 +7,6 @@ from .models import Event, Step
 class StepInline(admin.TabularInline):
   model = Step
   extra = 1
-  readonly_fields = ('created_by', 'created_at', 'updated_by', 'updated_at')
-
-  # TODO: Bu fonksiyon çalışmıyor. Override mı etmiyor. İncele.
-  def save_model(self, request, obj, form, change):
-    if obj.pk is None:
-      obj.created_by = request.user
-    else:
-      obj.updated_by = request.user
-
-    super().save_model(request, obj, form, change)
 
 
 class EventAdmin(admin.ModelAdmin):
