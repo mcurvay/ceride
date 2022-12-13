80306a35356c8cbc1f96f06c3f15afb8a1f2ae7c
Abuzer Emre Osmanoğlu <aeosmanoglu@icloud.com>
2022-12-13 07:34:46 +0300

    tab size 4

--- app/admin.py
+++ app/admin.py
@@ -2,33 +2,31 @@ from django.contrib import admin
 
 from .models import Event, Step
 
-# TODO: Admin panelin tema, dil, yerleşim ayarları yapılacak. Model netleştikten sonra.
-
 class StepInline(admin.TabularInline):
-  model = Step
-  extra = 1
+    model = Step
+    extra = 1
 
 
 class EventAdmin(admin.ModelAdmin):
-  inlines = [StepInline]
-  readonly_fields = ('created_by', 'created_at', 'updated_by', 'updated_at')
-  fieldsets = [
-    ('İlk Tespit', {'fields': ['title', 'description', 'dateTime', 'detection_method', 'detection_by', 'level', 'type']}),
-    ('Çözüm', {'fields': ['solved_at', 'source', 'solution_method']}),
-    ('Kayıt Bilgileri', {'fields': ['created_by', 'created_at', 'updated_by', 'updated_at']})
-    ]
-  list_display = ('title', 'dateTime', 'solved_at')
-  list_filter = ['dateTime', 'solved_at']
-  search_fields = ['title', 'description']
+    inlines = [StepInline]
+    readonly_fields = ('created_by', 'created_at', 'updated_by', 'updated_at')
+    fieldsets = [
+        ('İlk Tespit', {'fields': ['title', 'description', 'dateTime', 'detection_method', 'detection_by', 'level', 'type']}),
+        ('Çözüm', {'fields': ['solved_at', 'source', 'solution_method']}),
+        ('Kayıt Bilgileri', {'fields': ['created_by', 'created_at', 'updated_by', 'updated_at']})
+        ]
+    list_display = ('title', 'dateTime', 'solved_at')
+    list_filter = ['dateTime', 'solved_at']
+    search_fields = ['title', 'description']
 
 
-  def save_model(self, request, obj, form, change):
-    if obj.pk is None:
-      obj.created_by = request.user
-    else:
-      obj.updated_by = request.user
+    def save_model(self, request, obj, form, change):
+        if obj.pk is None:
+            obj.created_by = request.user
+        else:
+            obj.updated_by = request.user
 
-    super().save_model(request, obj, form, change)
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Event, EventAdmin)
--- app/models.py
+++ app/models.py
@@ -5,71 +5,71 @@ from django.contrib.auth.models import User
 # TODO: TextField veri girişlerinde markdown desteği eklenecek.
 
 LEVEL = [
-  (1, '1: Lorem ipsum'),
-  (2, '2: Dolor sit amet'),
-  (3, '3: Nöbetçi heyeti tarafından çözülemeyen ve geneli etkileyen, etkisi yüksek arıza')
-  ]
+    (1, '1: Lorem ipsum'),
+    (2, '2: Dolor sit amet'),
+    (3, '3: Nöbetçi heyeti tarafından çözülemeyen ve geneli etkileyen, etkisi yüksek arıza')
+    ]
 
 TYPE = [
-  (1, '1: Acilen müdahale edilmemesi durumunda donanımın kalıcı hasara uğraması'),
-  (2, '2: Kurumsal verinin kaybedilme ihtimali'),
-  (3, '3: Görevin yapılmasına, hizmetin vatandaşa sunulmasına mani bir durum oluşması'),
-  (4, '4: Kritik uygulamaların tamamen devredışı kalması'),
-  (5, '5: Kurumun imajını olumsuz etkileyecek hususlar'),
-  (6, '6: Diğer'),
-  ]
+    (1, '1: Acilen müdahale edilmemesi durumunda donanımın kalıcı hasara uğraması'),
+    (2, '2: Kurumsal verinin kaybedilme ihtimali'),
+    (3, '3: Görevin yapılmasına, hizmetin vatandaşa sunulmasına mani bir durum oluşması'),
+    (4, '4: Kritik uygulamaların tamamen devredışı kalması'),
+    (5, '5: Kurumun imajını olumsuz etkileyecek hususlar'),
+    (6, '6: Diğer'),
+    ]
 
 SOURCE = [
-  (1, 'Yazılım'),
-  (2, 'Donanım'),
-  (3, 'Altyapı'),
-  ]
+    (1, 'Yazılım'),
+    (2, 'Donanım'),
+    (3, 'Altyapı'),
+    ]
 
 SOLUTION = [
-  (1, 'Nöbetçi heyeti'),
-  (2, 'İlgili personel'),
-  (3, 'Firma Personeli')
-  ]
+    (1, 'Nöbetçi heyeti'),
+    (2, 'İlgili personel'),
+    (3, 'Firma Personeli')
+    ]
 
 class Event(models.Model): 
-  title = models.CharField('Olay', max_length=200)
-  description = models.TextField('Ön İnceleme')
-  dateTime = models.DateTimeField('Tespit Tarihi ve Saati', default=datetime.now)
-  detection_method = models.CharField('Tespit Yöntemi', max_length=200, help_text='Telefonla, SOME Ekranı, vb...')
-  detection_by = models.CharField('Tespit Eden', max_length=200, help_text='SOME Nöbetçi Heyeti')
-  level = models.IntegerField("Seviye", choices=LEVEL, default=3)
-  type = models.IntegerField("Tip", choices=TYPE, default=4)
-  source = models.IntegerField("Arıza Kaynağı", choices=SOURCE, null=True, blank=True)
-  solution_method = models.IntegerField("Çözüm Yöntemi", choices=SOLUTION, null=True, blank=True )
+    title = models.CharField('Olay', max_length=200)
+    description = models.TextField('Ön İnceleme')
+    dateTime = models.DateTimeField('Tespit Tarihi ve Saati', default=datetime.now)
+    detection_method = models.CharField('Tespit Yöntemi', max_length=200, help_text='Telefonla, SOME Ekranı, vb...')
+    detection_by = models.CharField('Tespit Eden', max_length=200, help_text='SOME Nöbetçi Heyeti')
+    level = models.IntegerField("Seviye", choices=LEVEL, default=3)
+    type = models.IntegerField("Tip", choices=TYPE, default=4)
+    source = models.IntegerField("Arıza Kaynağı", choices=SOURCE, null=True, blank=True)
+    solution_method = models.IntegerField("Çözüm Yöntemi", choices=SOLUTION, null=True, blank=True )
 
-  solved_at = models.DateTimeField(
-    'Çözüm Tarihi ve Saati', blank=True, null=True, default=None,
-    help_text='Çözüm tarihi ve saati girilmediyse olay çözülmedi olarak kabul edilecektir.'
-    )
+    solved_at = models.DateTimeField(
+        'Çözüm Tarihi ve Saati', blank=True, null=True, default=None,
+        help_text='Çözüm tarihi ve saati girilmediyse olay çözülmedi olarak kabul edilecektir.'
+        )
 
-  created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_created_by')
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_updated_by', null=True, blank=True)
-  updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_created_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_updated_by', null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-  class Meta:
-    verbose_name = 'Olay'
-    verbose_name_plural = 'Olaylar'
-    ordering = ['-dateTime']
+    class Meta:
+        verbose_name = 'Olay'
+        verbose_name_plural = 'Olaylar'
+        ordering = ['-dateTime']
 
 
 class Step(models.Model):
-  event = models.ForeignKey(Event, on_delete=models.CASCADE)
-  description = models.TextField('Yapılan İşlemler')
-  dateTime = models.DateTimeField('İşlem Tarihi ve Saati', default=datetime.now)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    description = models.TextField('Yapılan İşlemler')
+    dateTime = models.DateTimeField('İşlem Tarihi ve Saati', default=datetime.now)
 
-  def __str__(self):
-    return self.dateTime.strftime("%H:%M - %d.%m.%y")
+    def __str__(self):
+        return self.dateTime.strftime("%H:%M - %d.%m.%y")
 
-  class Meta:
-    verbose_name = 'Adım'
-    verbose_name_plural = 'Adımlar'
-    ordering = ['dateTime']
+    class Meta:
+        verbose_name = 'Adım'
+        verbose_name_plural = 'Adımlar'
+        ordering = ['dateTime']
