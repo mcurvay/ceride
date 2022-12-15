9ce793d2321d9f2548c8fac1dac90fc9d0d4db2a
Abuzer Emre Osmanoğlu <aeosmanoglu@icloud.com>
2022-12-15 21:27:02 +0000

    search backend

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import Event, Step
 
@@ -14,22 +15,17 @@ class EventListView(ListView):
     ordering = ['-dateTime']
     paginate_by = 10
 
-    def get_object(self, queryset=None):        
-        obj = super().get_object(queryset)
-        if obj is None:
-            raise Http404("Event does not exist")
-        return obj
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query is None: return Event.objects.all()
+        return Event.objects.filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        )
 
 class EventDetailView(DetailView):
     model = Event
     template_name = 'detail.html'
     context_object_name = 'event'
-
-    def get_object(self, queryset=None):
-        obj = super().get_object(queryset)
-        if obj is None:
-            raise Http404("Event does not exist")
-        return obj
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
