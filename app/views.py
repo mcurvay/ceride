1d557d83ad0d167d725049e18000a216256fd03d
ABUZER EMRE OSMANOĞLU <a.osmanoglu@jandarma.gov.tr>
2022-12-16 11:30:31 +0300

    arama formu eklendi

--- app/views.py
+++ app/views.py
@@ -1,26 +1,27 @@
-from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.db.models import Q
 
 from .models import Event, Step
 
-# TODO: index sayfası için arama formu eklenecek.
+
 # TODO: index sayfası için filtreleme formu eklenecek.
 # TODO: 404 sayfası eklenecek.
 
-class EventListView(ListView):    
+class EventListView(ListView):
     model = Event
     template_name = 'index.html'
     context_object_name = 'events'
     ordering = ['-dateTime']
-    paginate_by = 10
+    paginate_by = 12
 
     def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query is None: return Event.objects.all()
+        query = self.request.GET.get('ara')
+        if query is None:
+            return Event.objects.all()
         return Event.objects.filter(
             Q(title__icontains=query) | Q(description__icontains=query)
         )
+
 
 class EventDetailView(DetailView):
     model = Event
