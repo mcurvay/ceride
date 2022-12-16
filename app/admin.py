c6b910277cc2f853b366673b6332f90cf8c95a1f
ABUZER EMRE OSMANOĞLU <a.osmanoglu@jandarma.gov.tr>
2022-12-16 16:05:59 +0300

    model güncellemesi

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Event, Step
 
+
 class StepInline(admin.TabularInline):
     model = Step
     extra = 1
@@ -11,14 +12,14 @@ class EventAdmin(admin.ModelAdmin):
     inlines = [StepInline]
     readonly_fields = ('created_by', 'created_at', 'updated_by', 'updated_at')
     fieldsets = [
-        ('İlk Tespit', {'fields': ['title', 'description', 'dateTime', 'detection_method', 'detection_by', 'level', 'type']}),
-        ('Çözüm', {'fields': ['solved_at', 'source', 'solution_by','suggestions']}),
-        ('Kayıt Bilgileri', {'fields': ['created_by', 'created_at', 'updated_by', 'updated_at']})
-        ]
+        (None, {'fields': ['title', 'description', 'dateTime']}),
+        ('Tespit', {'fields': ['detection_method', 'detection_method_text', 'detection_by']}),
+        ('Etki', {'fields': ['level', 'type', 'type_text']}),
+        ('Çözüm', {'fields': ['solved_at', 'failure_source', 'solution_by', 'notes']}),
+        ('Kayıt Bilgileri', {'fields': ['created_by', 'created_at', 'updated_by', 'updated_at']})]
     list_display = ('title', 'dateTime', 'solved_at')
     list_filter = ['dateTime', 'solved_at']
     search_fields = ['title', 'description']
-
 
     def save_model(self, request, obj, form, change):
         if obj.pk is None:
--- app/models.py
+++ app/models.py
@@ -1,80 +1,145 @@
+import locale
+import pytz
 from datetime import datetime, timedelta
 
-import locale, pytz
-
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 locale.setlocale(locale.LC_ALL, "tr_TR.utf8")
 
 LEVEL = [
-    (1, '1: Lorem ipsum'),
-    (2, '2: Dolor sit amet'),
-    (3, '3: Nöbetçi heyeti tarafından çözülemeyen ve geneli etkileyen, etkisi yüksek arıza')
-    ]
+    (1, '1: Sınırlı etkili - Nöbet heyeti çözebilir'),
+    (2, '2: Sınırlı etkili - Nöbet heyeti çözemez'),
+    (3, '3: Geneli etkileyen etkisi yüksek arıza')]
 
 TYPE = [
-    (1, '1: Acilen müdahale edilmemesi durumunda donanımın kalıcı hasara uğraması'),
-    (2, '2: Kurumsal verinin kaybedilme ihtimali'),
-    (3, '3: Görevin yapılmasına, hizmetin vatandaşa sunulmasına mani bir durum oluşması'),
-    (4, '4: Kritik uygulamaların tamamen devredışı kalması'),
-    (5, '5: Kurumun imajını olumsuz etkileyecek hususlar'),
-    (6, '6: Diğer'),
-    ]
+    (1, '1: Donanımın kalıcı hasara uğrama riski'),
+    (2, '2: Kurumsal verinin kaybedilme riski'),
+    (3, '3: Görevin yapılamama/vatandaşa sunulamama riski'),
+    (4, '4: Kritik uygulamaların devredışı kalma riski'),
+    (5, '5: Kurumun imajını olumsuz etkileme riski'),
+    (6, '6: Diğer')]
 
-SOURCE = [
+DETECTION_METHOD = [
+    ('SOME İzleme Ekranları', 'SOME İzleme Ekranları'),
+    ('GÜVERCİNLİK İzleme Ekranları', 'GÜVERCİNLİK İzleme Ekranları'),
+    ('KASTAMONU İzleme Ekranı', 'KASTAMONU İzleme Ekranı'),
+    ('Kurum İçi', 'Kurum İçi (Diğer Birlikler, İç Hatlar, E-Posta, Telefon, vb.)'),
+    ('Kurum Dışı', 'Kurum Dışı')]
+
+FAILURE_SOURCE = [
     ('Yazılım', 'Yazılım'),
     ('Donanım', 'Donanım'),
     ('Konfigürasyon', 'Konfigürasyon'),
-    ]
+    ('Alt Yapı', 'Alt Yapı'),
+    ('Kurum Dışı', 'Kurum Dışı')]
 
-SOLUTIONBY = [
+SOLUTION_BY = [
     ('Nöbetçi heyeti', 'Nöbetçi heyeti'),
     ('İlgili personel', 'İlgili personel'),
-    ('Firma Personeli', 'Firma Personeli')
-    ]
+    ('Firma Personeli', 'Firma Personeli')]
 
-class Event(models.Model): 
-    title = models.CharField('Olay', max_length=200, help_text='Yeni olay oluşturmadan önce devam eden çözülmemiş olayları mutlaka kontrol ediniz. Olay başlığını belirlerken problemi doğru anlatan anahtar kelimeler kullanılmalıdır.')
-    description = models.TextField('Ön İnceleme')
-    dateTime = models.DateTimeField('Tespit Tarihi ve Saati', default=datetime.now)
-    detection_method = models.CharField('Tespit Yöntemi', max_length=200, help_text='Telefonla, SOME Ekranı, vb...')
-    detection_by = models.CharField('Tespit Eden', max_length=200, help_text='SOME Nöbetçi Heyeti')
-    level = models.IntegerField("Seviye", choices=LEVEL, default=3)
-    type = models.IntegerField("Tip", choices=TYPE, default=4)
-    suggestions = models.TextField('Öneriler', help_text='Problemin yeniden gerçekleşmemesi için alınabilecek tedbirler.', null=True, blank=True)
-    source = models.CharField("Arıza Kaynağı", choices=SOURCE, null=True, blank=True, max_length=15)
-    solution_by = models.CharField("Çözüm Geliştiren", choices=SOLUTIONBY, null=True, blank=True, max_length=20)
 
+class Event(models.Model):
+    # İlk Bilgiler
+    title = models.CharField(
+        'Olay *',
+        max_length=200,
+        help_text='''Yeni olay oluşturmadan önce devam eden çözülmemiş olayları mutlaka kontrol ediniz.
+        Olay başlığını belirlerken problemi doğru anlatan anahtar kelimeler kullanılmalıdır.''')
+    description = models.TextField('Ön İnceleme *')
+    dateTime = models.DateTimeField(
+        'Tespit Tarihi ve Saati *',
+        default=datetime.now)
+
+    # Tespit Bilgileri
+    detection_method = models.CharField(
+        'Tespit Yöntemi *',
+        choices=DETECTION_METHOD,
+        max_length=30)
+    detection_method_text = models.TextField(
+        'Tespit Yöntemi Açıklama',
+        null=True,
+        blank=True,
+        help_text='Tespit yönteminde ek açıklama alanı olarak kullanabilirsiniz.')
+    detection_by = models.CharField(
+        'Tespit Eden *',
+        max_length=200,
+        help_text='İlgili Birim, İsim Soyisim, Telefon, E-Posta, vb.')
+
+    # Etki Bilgileri
+    level = models.IntegerField(
+        'Seviye *',
+        choices=LEVEL,
+        default=3)
+    type = models.IntegerField(
+        'Tip *',
+        choices=TYPE,
+        default=4)
+    type_text = models.CharField(
+        'Tip Açıklama',
+        max_length=200,
+        null=True,
+        blank=True,
+        help_text='Tip 6 seçildiğinde ek açıklama alanı olarak kullanabilirsiniz.')
+
+    # Çözüm Bilgileri
     solved_at = models.DateTimeField(
-        'Çözüm Tarihi ve Saati', blank=True, null=True, default=None,
-        help_text='Çözüm tarihi ve saati girilmediyse olay çözülmedi olarak kabul edilecektir.'
-        )
+        'Çözüm Tarihi ve Saati',
+        blank=True,
+        null=True,
+        default=None,
+        help_text='Çözüm tarihi ve saati girilmediyse olay çözülmedi olarak kabul edilecektir.')
+    failure_source = models.CharField(
+        'Arıza Kaynağı',
+        choices=FAILURE_SOURCE,
+        null=True,
+        blank=True,
+        max_length=15)
+    solution_by = models.CharField(
+        'Çözüm Geliştiren',
+        choices=SOLUTION_BY,
+        null=True,
+        blank=True,
+        max_length=20)
+    notes = models.TextField(
+        'Ek Açıklama',
+        null=True,
+        blank=True,
+        help_text='Komutan emri, bilir kişi raporu, öneriler vb. ek açıklamaları buraya yazabilirsiniz.')
 
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_created_by')
+    # Otomatik Bilgiler
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='%(class)s_created_by')
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='%(class)s_updated_by', null=True,
+        blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_updated_by', null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
     def formatted_date(self):
-        dateTime = self.dateTime.astimezone(pytz.timezone('Europe/Istanbul'))
-        if dateTime.date() == datetime.now().date():
-            return 'Bugün ' + dateTime.strftime("%H:%M")
-        elif dateTime.date() == datetime.now().date() - timedelta(days=1):
-            return 'Dün ' + dateTime.strftime("%H:%M")
-        elif dateTime.date() >= datetime.now().date() - timedelta(days=7):
-            return dateTime.strftime("%A")
-        elif dateTime.year == datetime.now().year:
-            return dateTime.strftime("%d %B")
+        date_time = self.dateTime.astimezone(pytz.timezone('Europe/Istanbul'))
+        if date_time.date() == datetime.now().date():
+            return 'Bugün ' + date_time.strftime("%H:%M")
+        elif date_time.date() == datetime.now().date() - timedelta(days=1):
+            return 'Dün ' + date_time.strftime("%H:%M")
+        elif date_time.date() >= datetime.now().date() - timedelta(days=7):
+            return date_time.strftime("%A")
+        elif date_time.year == datetime.now().year:
+            return date_time.strftime("%d %B")
         else:
-            return dateTime.strftime("%d %B %Y")
+            return date_time.strftime("%d %B %Y")
 
     def is_today(self):
-        dateTime = self.dateTime.astimezone(pytz.timezone('Europe/Istanbul'))
-        return dateTime.date() == datetime.now().date()
+        date_time = self.dateTime.astimezone(pytz.timezone('Europe/Istanbul'))
+        return date_time.date() == datetime.now().date()
 
     class Meta:
         verbose_name = 'Olay'
@@ -88,15 +153,15 @@ class Step(models.Model):
     dateTime = models.DateTimeField('İşlem Tarihi ve Saati', default=datetime.now)
 
     def __str__(self):
-        dateTime = self.dateTime.astimezone(pytz.timezone('Europe/Istanbul'))
-        return dateTime.strftime("%H:%M - %d %B %y %A")
+        date_time = self.dateTime.astimezone(pytz.timezone('Europe/Istanbul'))
+        return date_time.strftime("%H:%M - %d %B %y %A")
 
     def formatted_date(self):
-        dateTime = self.dateTime.astimezone(pytz.timezone('Europe/Istanbul'))
+        date_time = self.dateTime.astimezone(pytz.timezone('Europe/Istanbul'))
         if self.dateTime.date() == self.event.dateTime.date():
-            return dateTime.strftime("%H:%M")
+            return date_time.strftime("%H:%M")
         else:
-            return dateTime.strftime("%d %B %y - %H:%M")
+            return date_time.strftime("%d %B %y - %H:%M")
 
     class Meta:
         verbose_name = 'Adım'
